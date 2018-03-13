1dba6d74a487fac73977d8f3db2c16dd4686d3e6
= <robin.white280@gmail.com>
2018-03-13 22:52:48 +0000

    updated test

--- test/SpiderTest.java
+++ test/SpiderTest.java
@@ -10,30 +10,43 @@ public class SpiderTest {
     @Test
     public void WWWReturnsFullAddress() {
         Spider spider = new Spider();
-        assertEquals("www.google.com",spider.transformLink("www.google.com"));
+        assertEquals("www.google.com",spider.transformLink("www.google.com/images","www.google.com"));
     }
 
     @Test
     public void HttpsReturnsHttpsAddress() {
         Spider spider = new Spider();
-        assertEquals("https://www.google.com/images",spider.transformLink("https://www.google.com/images"));
+        assertEquals("https://www.google.com/images",spider.transformLink("www.google.com/images","https://www.google.com/images"));
     }
 
     @Test
     public void DoubleSlashReturnsFullAddress() {
         Spider spider = new Spider();
-        assertEquals("www.google.com/images",spider.transformLink("//google.com/images"));
+        assertEquals("www.google.com/images",spider.transformLink("www.google.com/images","//google.com/images"));
     }
 
     @Test
     public void HashReturnsAnchorAddress() {
         Spider spider = new Spider();
-        assertEquals("www.google.com/images/#anchor",spider.transformLink("#anchor"));
+        assertEquals("www.google.com/images#anchor",spider.transformLink("www.google.com/images","#anchor"));
     }
 
     @Test
     public void DotDotSlashReturnsFullAddress() {
         Spider spider = new Spider();
-        assertEquals("www.google.com/images/#anchor",spider.transformLink("#anchor"));
+        assertEquals("www.google.com/",spider.transformLink("www.google.com/images","../"));
     }
+
+    @Test
+    public void EmptyReturnsNull(){
+        Spider spider = new Spider();
+        assertEquals(null,spider.transformLink("www.google.com",""));
+    }
+
+    @Test
+    public void NullSReturnsNull(){
+        Spider spider = new Spider();
+        assertEquals(null,spider.transformLink("www.google.com",null));
+    }
+
 }
